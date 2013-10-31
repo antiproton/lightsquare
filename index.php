1b19082dca101c8f0a318677501c61d53e025cce
Gus <springheeljim@gmail.com>
2013-10-31 13:11:32 +0000

    initial commit - adds index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once "base.php";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Chess</title>
+		<style type="text/css">
+		<?php
+		loads("/lib/css/reset.css");
+		loads("/lib/css/common.css");
+		?>
+		</style>
+		<script type="text/javascript">
+		<?php
+		loads("/lib/js/util");
+		loads("/lib/js/data");
+		loads("/lib/js/server");
+		loads("/lib/js/events");
+		loads("/lib/js/dom");
+		loads("/lib/js/base");
+		loads("/lib/js/Base.js");
+		?>
+		</script>
+	</head>
+	<body>
+
+	</body>
+</html>
