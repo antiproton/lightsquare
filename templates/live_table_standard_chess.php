acdcf49bbf7c10443e9de4560212cbf52bc145aa
Gus <springheeljim@gmail.com>
2013-11-09 20:40:50 +0000

    adds templates

---
+++ templates/live_table_standard_chess.php
@@ -0,0 +1,50 @@
+<div class="template" id="live_table_standard_chess">
+	<div class="live_table_standard_chess">
+		<div class="main">
+			<div class="player">
+				<div class="player_info" data-id="player_info_opponent">
+
+				</div>
+				<div class="player_clock" data-id="player_clock_opponent">
+
+				</div>
+			</div>
+			<div class="board" data-id="board">
+
+			</div>
+			<div class="player">
+				<div class="player_info" data-id="player_info_player">
+
+				</div>
+				<div class="player_clock" data-id="player_clock_player">
+
+				</div>
+			</div>
+			<div class="table_chat">
+				<div class="chat_box" data-id="table_chat_message_list">
+
+				</div>
+				<div class="chat_controls">
+					<input type="text" class="table_chat_message" data-id="table_chat_message">
+					<input type="button" class="table_chat_send" data-id="table_chat_send">
+				</div>
+			</div>
+		</div>
+		<div class="util">
+			<div class="history" data-id="history">
+
+			</div>
+			<div class="history_controls" data-id="history_controls">
+
+			</div>
+			<div class="buttons">
+				<a class="button" data-id="button_ready">Ready</a>
+				<a class="button" data-id="button_stand">Stand</a>
+				<a class="button" data-id="button_resign">Resign</a>
+			</div>
+			<div class="pieces_taken" data-id="pieces_taken">
+				
+			</div>
+		</div>
+	</div>
+</div>
