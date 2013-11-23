07ee08e8e68f1f072414a876bd838ef873e70c89
Gus <springheeljim@gmail.com>
2013-11-23 20:02:39 +0000

    switches to Template.SetStyle for CSS - adds script tag and changes class attrs to data-class

--- templates/live_table_std.php
+++ templates/live_table_std.php
@@ -1,51 +1,69 @@
+<script type="text/javascript">
+Template.SetStyles("live_table_std", {
+	details: {
+		float: "left",
+		width: 150,
+		border: "1px solid red"
+	},
+
+	main: {
+		float: "left"
+	},
+
+	util: {
+		float: "left",
+		width: 150
+	}
+});
+</script>
 <div class="template" id="live_table_std">
-	<div class="live_table_std" data-id="root">
-		<div class="details">
-
+	<div data-class="root" data-id="root">
+		<div data-class="details">
+			asd
 		</div>
-		<div class="main">
-			<div class="player">
-				<div class="player_info" data-id="player_info_opponent">
+		<div data-class="main">
+			<div data-class="player">
+				<div data-class="player_info" data-id="player_info_opponent">
 
 				</div>
-				<div class="player_clock" data-id="player_clock_opponent">
-
-				</div>
-			</div>
-			<div class="board" data-id="board">
-
-			</div>
-			<div class="player">
-				<div class="player_info" data-id="player_info_player">
-
-				</div>
-				<div class="player_clock" data-id="player_clock_player">
+				<div data-class="player_clock" data-id="player_clock_opponent">
 
 				</div>
 			</div>
-			<div class="table_chat">
-				<div class="chat_box" data-id="table_chat_message_list">
+			<div data-class="board" data-id="board">
+
+			</div>
+			<div data-class="player">
+				<div data-class="player_info" data-id="player_info_player">
 
 				</div>
-				<div class="chat_controls">
-					<input type="text" class="table_chat_message" data-id="table_chat_message">
-					<input type="button" class="table_chat_send" data-id="table_chat_send" value="Send">
+				<div data-class="player_clock" data-id="player_clock_player">
+
+				</div>
+			</div>
+			<div data-class="table_chat">
+				<div data-class="chat_box" data-id="table_chat_message_list">
+
+				</div>
+				<div data-class="chat_controls">
+					<input type="text" data-class="table_chat_message" data-id="table_chat_message">
+					<input type="button" data-class="table_chat_send" data-id="table_chat_send" value="Send">
 				</div>
 			</div>
 		</div>
-		<div class="util">
-			<div class="history" data-id="history">
+		<div data-class="util">
+			<div data-class="history" data-id="history">
 
 			</div>
-			<div class="history_controls" data-id="history_controls">
+			<div data-class="history_controls" data-id="history_controls">
 
 			</div>
-			<div class="buttons">
+			<div data-class="buttons">
 				<a class="button" href="javascript:void(0)" data-id="button_ready">Ready</a>
 				<a class="button" href="javascript:void(0)" data-id="button_stand">Stand</a>
 				<a class="button" href="javascript:void(0)" data-id="button_resign">Resign</a>
 			</div>
-			<div class="pieces_taken" data-id="pieces_taken">
+			<div data-class="pieces_taken" data-id="pieces_taken">
 
 			</div>
 		</div>
