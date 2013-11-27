a79131c8d40a0bb5ef00df6cda55fd73c056ce75
Gus <springheeljim@gmail.com>
2013-11-27 23:18:19 +0000

    updates argument order of Template constructor

--- js/uis/LiveTableStdUi.js
+++ js/uis/LiveTableStdUi.js
@@ -3,7 +3,7 @@ user interface for a standard chess live table
 */
 
 function LiveTableStdUi(parent) {
-	this.tpl=new Template(parent, "live_table_std");
+	this.tpl=new Template("live_table_std", parent);
 
 	this.PlayerInfo={
 		Player: new PlayerInfo(this.tpl.player_info_player),
