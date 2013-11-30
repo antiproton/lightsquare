5a90100ba89ae67454919077cb7f5f2eddc74a74
Gus <springheeljim@gmail.com>
2013-11-30 11:21:25 +0000

    removes Template.Style and changes data-class to prefixed class

--- templates/live_table_std.php
+++ templates/live_table_std.php
@@ -1,69 +1,51 @@
-<script type="text/javascript">
-Template.SetStyles("live_table_std", {
-	details: {
-		float: "left",
-		width: 150,
-		border: "1px solid red"
-	},
-
-	main: {
-		float: "left"
-	},
-
-	util: {
-		float: "left",
-		width: 150
-	}
-});
-</script>
 <div class="template" id="live_table_std">
-	<div data-class="root" data-id="root">
-		<div data-class="details">
+	<div class="live_table_std_root" data-id="root">
+		<div class="live_table_std_details">
 			asd
 		</div>
-		<div data-class="main">
-			<div data-class="player">
-				<div data-class="player_info" data-id="player_info_opponent">
+		<div class="live_table_std_main">
+			<div class="live_table_std_player">
+				<div class="live_table_std_player_info" data-id="player_info_opponent">
 
 				</div>
-				<div data-class="player_clock" data-id="player_clock_opponent">
-
-				</div>
-			</div>
-			<div data-class="board" data-id="board">
-
-			</div>
-			<div data-class="player">
-				<div data-class="player_info" data-id="player_info_player">
-
-				</div>
-				<div data-class="player_clock" data-id="player_clock_player">
+				<div class="live_table_std_player_clock" data-id="player_clock_opponent">
 
 				</div>
 			</div>
-			<div data-class="table_chat">
-				<div data-class="chat_box" data-id="table_chat_message_list">
+			<div class="live_table_std_board" data-id="board">
+
+			</div>
+			<div class="live_table_std_player">
+				<div class="live_table_std_player_info" data-id="player_info_player">
 
 				</div>
-				<div data-class="chat_controls">
-					<input type="text" data-class="table_chat_message" data-id="table_chat_message">
-					<input type="button" data-class="table_chat_send" data-id="table_chat_send" value="Send">
+				<div class="live_table_std_player_clock" data-id="player_clock_player">
+
+				</div>
+			</div>
+			<div class="live_table_std_table_chat">
+				<div class="live_table_std_chat_box" data-id="table_chat_message_list">
+
+				</div>
+				<div class="live_table_std_chat_controls">
+					<input type="text" class="live_table_std_table_chat_message" data-id="table_chat_message">
+					<input type="button" class="live_table_std_table_chat_send" data-id="table_chat_send" value="Send">
 				</div>
 			</div>
 		</div>
-		<div data-class="util">
-			<div data-class="history" data-id="history">
+		<div class="live_table_std_util">
+			<div class="live_table_std_history" data-id="history">
 
 			</div>
-			<div data-class="history_controls" data-id="history_controls">
+			<div class="live_table_std_history_controls" data-id="history_controls">
 
 			</div>
-			<div data-class="buttons">
+			<div class="live_table_std_buttons">
 				<a class="button" href="javascript:void(0)" data-id="button_ready">Ready</a>
 				<a class="button" href="javascript:void(0)" data-id="button_stand">Stand</a>
 				<a class="button" href="javascript:void(0)" data-id="button_resign">Resign</a>
 			</div>
-			<div data-class="pieces_taken" data-id="pieces_taken">
+			<div class="live_table_std_pieces_taken" data-id="pieces_taken">
 
 			</div>
 		</div>
