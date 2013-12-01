ac2799db2ef32e0b7abf2803e4578601fe0a8e2a
Gus <springheeljim@gmail.com>
2013-12-01 17:08:05 +0000

    adds isFullmoveDisplayed method (returns false) to MoveColView

--- widgets/History/move/MoveColView.js
+++ widgets/History/move/MoveColView.js
@@ -1,6 +1,11 @@
 function MoveColView() {
 	UiMove.implement(this);
+
 	this._tpl=new Template("move_colview");
 	this.node=this._tpl.root;
 	this.parentFullmove=null;
+}
+
+MoveColView.prototype.isFullmoveDisplayed=function() {
+	return false;
 }
