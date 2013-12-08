c8460a78d86659c4ce5c137954fe4bff39d8d3a6
Gus <springheeljim@gmail.com>
2013-12-08 19:05:40 +0000

    renames _tpl to _template and updates reference from UiBoard to BoardWidget in UiLiveTableStandard

--- widgets/LiveTableStandard/UiLiveTableStandard.js
+++ widgets/LiveTableStandard/UiLiveTableStandard.js
@@ -1,16 +1,16 @@
 function UiLiveTableStandard(parent) {
-	this._tpl=new Template("live_table_standard", parent);
+	this._template=new Template("live_table_standard", parent);
 
 	this.playerInfo={
-		player: new PlayerInfo(this._tpl.player_info_player),
-		opponent: new PlayerInfo(this._tpl.player_info_opponent)
+		player: new PlayerInfo(this._template.player_info_player),
+		opponent: new PlayerInfo(this._template.player_info_opponent)
 	};
 
 	this.playerClock={
-		player: new PlayerClock(this._tpl.player_clock_player),
-		opponent: new PlayerClock(this._tpl.player_clock_opponent)
+		player: new PlayerClock(this._template.player_clock_player),
+		opponent: new PlayerClock(this._template.player_clock_opponent)
 	};
 
-	this.board=new UiBoard(this._tpl.board);
-	this.history=new HistoryColView(this._tpl.history);
+	this.board=new BoardWidget(this._template.board);
+	this.history=new HistoryColView(this._template.history);
 }
