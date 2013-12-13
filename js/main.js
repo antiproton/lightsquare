72dbcf38169ebd1a61e4e9dd1a35372114236296
Gus <springheeljim@gmail.com>
2013-12-13 20:25:53 +0000

    adds main.js for AMD testing

---
+++ js/main.js
@@ -0,0 +1,23 @@
+define(function(require) {
+	var LiveTableStandard=require("./widgets/LiveTableStandard/index");
+	var g=require("js/g");
+	var tableWidget=new LiveTableStandard(g("table"));
+
+
+
+	tableWidget.board.setSquareSize(45);
+
+	var game=new Game();
+
+	tableWidget.board.setBoardArray(game.position.board.getBoardArray());
+
+	var history=tableWidget.history;
+
+	var move=history.createMove();
+	var label=new MoveLabel();
+
+	label.piece="N";
+	move.setLabel(label);
+
+	history.move(move);
+});
