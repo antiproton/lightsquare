43a3fb98ad06f5195c57e7b5ef4f763b8236d8a9
Gus <springheeljim@gmail.com>
2013-12-28 18:05:25 +0000

    renames module value to Variation in colview variation

--- js/widgets/history/colview/_Variation/Variation.js
+++ js/widgets/history/colview/_Variation/Variation.js
@@ -1,23 +1,23 @@
 define(function(require) {
-	var Variation=require("chess/history/Variation");
+	var Base=require("chess/history/Variation");
 	var MoveList=require("./_MoveList/MoveList");
 	var Template=require("lib/dom/Template");
 	var html=require("file@./resources/variation.html");
 	require("css@./resources/variation.css");
 
-	function Class() {
-		Variation.call(this);
+	function Variation() {
+		Base.call(this);
 
 		this._template=new Template(html);
 		this._template.root.appendChild(this.moveList.node);
 		this.node=this._template.root;
 	}
 
-	Class.implement(Variation);
+	Variation.implement(Base);
 
-	Class.prototype._createMoveList=function() {
+	Variation.prototype._createMoveList=function() {
 		return new MoveList();
 	}
 
-	return Class;
+	return Variation;
 });
