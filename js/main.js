f93a213a6c8fc6e4f6b70675a47129b8d9455690
Gus <springheeljim@gmail.com>
2013-12-28 18:04:50 +0000

    gets test working in main

--- js/main.js
+++ js/main.js
@@ -8,18 +8,25 @@ define(function(require) {
 	var g=require("lib/dom/byId");
 	var MoveLabel=require("chess/MoveLabel");
 	var Board=require("widgets/board/Board");
+	var Position=require("chess/Position");
+	var Piece=require("chess/Piece");
 
+	var game=new Game();
 	var table=new Table(g("table"));
+
+	var position=new Position();
+	position.active=Piece.BLACK;
+	position.fullmove=30;
+
+	game.setStartingFen(position.getFen())
+
+	var move=game.move(48, 40);
+	var historyMove=table.history.createMove(move);
+
+	table.history.move(historyMove);
 
 	table.board.setSquareSize(60);
 	table.board.setShowSurround(false);
 	table.board.setSquareStyle(Board.squareStyles.green);
 	table.board.setBoardArray(game.position.board.getBoardArray());
-
-	var game=new Game();
-	var history=table.history;
-	var move=game.move(8, 16);
-	var historyMove=history.createMove(move);
-	
-	history.move(move);
 });
