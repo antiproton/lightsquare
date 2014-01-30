5bfe6d888c53db500adefe17828721fd9e4b16fb
Gus <springheeljim@gmail.com>
2014-01-30 20:16:54 +0000

    adds some code to client and server Game classes

--- js/client/Game.js
+++ js/client/Game.js
@@ -1,8 +1,17 @@
 define(function(require) {
 	var ChessGame=require("chess/Game");
+	var Piece=require("chess/Piece");
+	var Move=require("chess/Move");
 
-	function Game(id, socket) {
-		this._id=id;
+	function Game(server, details) {
+		this._id=details.id;
+		
+		this._players=[];
+		this._players[Piece.WHITE]=details.white;
+		this._players[Piece.BLACK]=details.black;
+		
+		this._rated=details.rated;
+		
 		this._game=new ChessGame();
 	}
 
