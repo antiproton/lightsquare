4d0192ab41f91a7cf763e95cd48b67cfb0f3277c
Gus <springheeljim@gmail.com>
2014-02-28 18:51:01 +0000

    moves tests out of main, adds LightSquareUi widget for the main interface and empty Application module for the client application

--- js/client/main.js
+++ js/client/main.js
@@ -1,42 +1,12 @@
 define(function(require) {
 	require("lib/dom/ready!");
 	var Server = require("lib/websocket-client/Server");
-	var $ = require("lib/dom/byId");
-	var TabControl = require("lib/widgets/TabControl/TabControl");
-	var Game = require("chess/Game");
-	var Piece = require("chess/Piece");
-	var Chess = require("chess/Chess");
+	var Application = require("./Application");
+	var LightSquareUi = require("./widgets/LightSquareUi/LightSquareUi");
 	
-	server = new Server("ws://chess:8080");
+	var server = new Server("ws://chess:8080");
+	var app = new Application(server);
+	var ui = new LightSquareUi(document.body, app);
+	
 	server.connect();
-	
-	var game = new Game();
-	
-	function move(from, to, promote) {
-		console.log(game.move(from, to, promote).getLabel().toString());
-		console.log(game.getPosition().getFen());
-	}
-	
-	for(var i=0; i<64; i++) {
-		window[Chess.algebraicFromSquare(i)]=i;
-	}
-	
-	move(e2, e4);
-	move(g8, f6);
-	move(b1, c3);
-	move(d7, d5);
-	move(f1, c4);
-	move(b8, c6);
-	move(d1, f3);
-	move(f6, h5);
-	move(f3, f7);
-	move(e8, d7);
-	
-	console.log(game.getPosition().countLegalMoves(Piece.BLACK));
-	
-	server.subscribe("*", function(url, data) {
-		if(url !== "/heartbeat") {
-			console.log(url, data);
-		}
-	});
 });
