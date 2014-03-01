50d4491efaf6463f86d7bb3a75a5d4af6887b0bd
Gus <springheeljim@gmail.com>
2014-03-01 23:28:04 +0000

    adds html and css to lightsquare_ui widget

--- js/client/widgets/LightsquareUi/LightsquareUi.js
+++ js/client/widgets/LightsquareUi/LightsquareUi.js
@@ -1,6 +1,11 @@
 define(function(require) {
-	function LightSquareUi() {
-		
+	require("css!./resources/lightsquare_ui.css");
+	var html = require("file!./resources/lightsquare_ui.html");
+	var Template = require("lib/dom/Template");
+	
+	function LightSquareUi(parent, app) {
+		this._app = app;
+		this._template = new Template(html, parent);
 	}
 	
 	return LightSquareUi;
