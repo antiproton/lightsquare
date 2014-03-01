f6fb70e7098b56a0faf3eec087f01235c6447d6b
Gus <springheeljim@gmail.com>
2014-03-01 14:35:10 +0000

    corrects reference to LightsquareUi in client main

--- js/client/main.js
+++ js/client/main.js
@@ -2,7 +2,7 @@ define(function(require) {
 	require("lib/dom/ready!");
 	var Server = require("lib/websocket-client/Server");
 	var Application = require("./Application");
-	var LightSquareUi = require("./widgets/LightsquareUi/LightsquareUi");
+	var LightsquareUi = require("./widgets/LightsquareUi/LightsquareUi");
 	
 	var server = new Server("ws://chess:8080");
 	var app = new Application(server);
