776b147d9c6e173fb5b82e426848c47358c9693f
Gus <springheeljim@gmail.com>
2014-03-01 00:39:30 +0000

    renames LightSquareUi to LightsquareUi

--- js/client/main.js
+++ js/client/main.js
@@ -2,11 +2,11 @@ define(function(require) {
 	require("lib/dom/ready!");
 	var Server = require("lib/websocket-client/Server");
 	var Application = require("./Application");
-	var LightSquareUi = require("./widgets/LightSquareUi/LightSquareUi");
+	var LightSquareUi = require("./widgets/LightsquareUi/LightsquareUi");
 	
 	var server = new Server("ws://chess:8080");
 	var app = new Application(server);
-	var ui = new LightSquareUi(document.body, app);
+	var ui = new LightsquareUi(document.body, app);
 	
 	server.connect();
 });
