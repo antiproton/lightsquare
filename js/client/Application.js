31fb1871b096519e9ef3dab502ec17403b4bde6f
Gus <springheeljim@gmail.com>
2014-03-01 14:35:31 +0000

    adds to and refactors challenge code
    adds NewChallenge event in client Application.  removes /challenge/list and makes /challenge/new
    always an array.  adds client Challenge. changes /challenge/expired {id} to /challenge/expired/id.
    adds the rest of the challenge properties to server Challenge toJSON.

--- js/client/Application.js
+++ js/client/Application.js
@@ -1,12 +1,22 @@
 define(function(require) {
+	var Challenge = require("./Challenge");
+	
 	function Application(server) {
 		this._server = server;
 		
+		this.NewChallenge = new Event(this);
 		
+		this._server.subscribe("/challenge/new", (function(challenges) {
+			challenges.forEach((function(challenge) {
+				this.NewChallenge.fire({
+					challenge: new Challenge(this._server, challenge)
+				});
+			}).bind(this));
+		}).bind(this));
 	}
 	
 	Application.prototype.createChallenge = function(options) {
-		
+		this._server.send("/challenge/create", options);
 	}
 	
 	return Application;
