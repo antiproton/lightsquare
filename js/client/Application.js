ceb93cbf1db3ce50b0591223837b5df94eeecca4
Gus <springheeljim@gmail.com>
2014-03-03 22:19:14 +0000

    removes 'table' concept
    Table widget renamed to Game, and Application and Challenge changed
    accordingly.  adds _spectators to server Game.

--- js/client/Application.js
+++ js/client/Application.js
@@ -1,10 +1,12 @@
 define(function(require) {
 	var Challenge = require("./Challenge");
+	var Game = require("./Game");
 	
 	function Application(server) {
 		this._server = server;
 		
 		this.NewChallenge = new Event(this);
+		this.NewGame = new Event(this);
 		
 		this._server.subscribe("/challenge/new", (function(challenges) {
 			challenges.forEach((function(challenge) {
@@ -12,6 +14,12 @@ define(function(require) {
 					challenge: new Challenge(this._server, challenge)
 				});
 			}).bind(this));
+		}).bind(this));
+		
+		this._server.subscribe("/game/new", (function(game) {
+			this.NewGame.fire({
+				game: new Game(this._server, game)
+			});
 		}).bind(this));
 	}
 	
--- js/client/widgets/Game/Game.js
+++ js/client/widgets/Game/Game.js
@@ -2,16 +2,16 @@ define(function(require) {
 	var Template = require("lib/dom/Template");
 	var Board = require("widgets/Board/Board");
 	var History = require("widgets/History/History");
-	var html = require("file!./resources/table.html");
-	require("css!./resources/table.css");
+	var html = require("file!./resources/game.html");
+	require("css!./resources/game.css");
 
-	function Table(table, parent) {
-		this._table = table;
+	function Game(game, parent) {
+		this._game = game;
 		this._template = new Template(html, parent);
 
 		this._board = new Board(this._template.board);
 		this._history = new History(this._template.history);
 	}
 
-	return Table;
+	return Game;
 });
