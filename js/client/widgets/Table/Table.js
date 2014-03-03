802f02811ddce61babb5bb9f242525c6fd5d6e9c
Gus <springheeljim@gmail.com>
2014-03-03 21:46:09 +0000

    Table widget - makes variables private, adds table parameter, removes commented code

--- js/client/widgets/Table/Table.js
+++ js/client/widgets/Table/Table.js
@@ -5,21 +5,12 @@ define(function(require) {
 	var html = require("file!./resources/table.html");
 	require("css!./resources/table.css");
 
-	function Table(parent) {
+	function Table(table, parent) {
+		this._table = table;
 		this._template = new Template(html, parent);
 
-		//this.playerInfo = {
-		//	player: new PlayerInfo(this._template.player_info_player),
-		//	opponent: new PlayerInfo(this._template.player_info_opponent)
-		//};
-		//
-		//this.playerClock = {
-		//	player: new PlayerClock(this._template.player_clock_player),
-		//	opponent: new PlayerClock(this._template.player_clock_opponent)
-		//};
-
-		this.board = new Board(this._template.board);
-		this.history = new History(this._template.history);
+		this._board = new Board(this._template.board);
+		this._history = new History(this._template.history);
 	}
 
 	return Table;
