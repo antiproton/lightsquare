00861249641e7ef485a1005720e98be02186c5f4
Gus <springheeljim@gmail.com>
2014-03-03 21:45:29 +0000

    adds code to client Game.move; adds PromotionPieceNeeded event

--- js/client/Game.js
+++ js/client/Game.js
@@ -1,22 +1,48 @@
 define(function(require) {
 	var ChessGame = require("chess/Game");
 	var Piece = require("chess/Piece");
-	var Move = require("chess/Move");
+	var ChessMove = require("chess/Move");
+	var Move = require("./Move");
 
-	function Game(server, details) {
-		this._id = details.id;
+	function Game(server, game) {
+		this.PromotionPieceNeeded = new Event(this);
+		
+		this._server = server;
+		
+		this._id = game.id;
 		
 		this._players = [];
-		this._players[Piece.WHITE] = details.white;
-		this._players[Piece.BLACK] = details.black;
+		this._players[Piece.WHITE] = game.white;
+		this._players[Piece.BLACK] = game.black;
 		
-		this._rated = details.rated;
+		this._isRated = game.isRated;
+		
+		this._history = [];
+		
+		game.history.forEach((function(move) {
+			this._history.push(Move.fromJSON(move));
+		}).bind(this));
 		
 		this._game = new ChessGame();
 	}
 
-	Game.prototype.move = function(from, to) {
-
-		var move = new Move(this._game.getPosition(), from, to, QUEEN);
+	Game.prototype.move = function(from, to, promoteTo) {
+		var move = new ChessMove(this._game.getPosition(), from, to, promoteTo);
+		
+		if(move.isPromotion() && promoteTo === undefined) {
+			this.PromotionPieceNeeded.fire();
+		}
+		
+		else {
+			this._history.push(move);
+			
+			this._server.send("/game/" + this._id + "/move", {
+				from: from,
+				to: to,
+				promoteTo: promoteTo
+			});
+		}
 	}
+	
+	return Game;
 });
