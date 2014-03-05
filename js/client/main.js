81b9165f0d3273996b7fffcb7af7a800dfbc30f5
Gus <springheeljim@gmail.com>
2014-03-05 21:44:02 +0000

    changes "chess" to window.location.hostname for websocket server

--- js/client/main.js
+++ js/client/main.js
@@ -4,7 +4,7 @@ define(function(require) {
 	var Application = require("./Application");
 	var LightsquareUi = require("./widgets/LightsquareUi/LightsquareUi");
 	
-	var server = new Server("ws://chess:8080");
+	var server = new Server("ws://" + window.location.hostname + ":8080");
 	var app = new Application(server);
 	var ui = new LightsquareUi(document.body, app);
 	
