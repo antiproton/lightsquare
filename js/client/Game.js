c89757c98cdf6e2993df96c58083ff759db996c0
Gus <springheeljim@gmail.com>
2014-03-06 22:26:33 +0000

    adds move queue to client Game and adds _options
    moveQueue makes sure moves received from the server are added to the history
    in the right order even if they're received out of order.

--- js/client/Game.js
+++ js/client/Game.js
@@ -6,42 +6,74 @@ define(function(require) {
 
 	function Game(server, game) {
 		this.PromotionPieceNeeded = new Event(this);
+		this.MoveReceived = new Event(this);
 		
 		this._server = server;
-		
 		this._id = game.id;
-		
 		this._players = [];
 		this._players[Piece.WHITE] = game.white;
 		this._players[Piece.BLACK] = game.black;
-		
-		this._isRated = game.isRated;
-		
+		this._options = game.options;
+		this._game = new ChessGame(this._options);
 		this._history = [];
+		this._moveQueue = [];
 		
 		game.history.forEach((function(move) {
 			this._history.push(Move.fromJSON(move));
 		}).bind(this));
 		
-		this._game = new ChessGame();
+		this._server.subscribe("/game/" + this._id + "/move", (function(move) {
+			this._serverMove(move);
+		}).bind(this));
 	}
 
 	Game.prototype.move = function(from, to, promoteTo) {
-		var move = new ChessMove(this._game.getPosition(), from, to, promoteTo);
+		var move = this._game.move(from, to, promoteTo);
 		
-		if(move.isPromotion() && promoteTo === undefined) {
-			this.PromotionPieceNeeded.fire();
+		if(move.isLegal()) {
+			if(move.isPromotion() && promoteTo === undefined) {
+				this._game.undoLastMove();
+				this.PromotionPieceNeeded.fire();
+			}
+			
+			else {
+				this._history.push(move);
+				
+				this._server.send("/game/" + this._id + "/move", {
+					from: from,
+					to: to,
+					promoteTo: promoteTo
+				});
+			}
+		}
+		
+		return move;
+	}
+	
+	Game.prototype._serverMove = function(move) {
+		if(move.index > this._history.length) {
+			this._moveQueue[move.index] = move;
 		}
 		
 		else {
-			this._history.push(move);
+			this._applyServerMove(move);
 			
-			this._server.send("/game/" + this._id + "/move", {
-				from: from,
-				to: to,
-				promoteTo: promoteTo
-			});
+			var i = move.index;
+			var nextMove;
+			
+			while(nextMove = this._moveQueue[++i]) {
+				this._applyServerMove(nextMove);
+			}
 		}
+	}
+	
+	Game.prototype._applyServerMove = function(move) {
+		this._game.move(move.from, move.to, move.promoteTo);
+		this._history.push(move);
+		
+		this.MoveReceived.fire({
+			move: move
+		});
 	}
 	
 	return Game;
