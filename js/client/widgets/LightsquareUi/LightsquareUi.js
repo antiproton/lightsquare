e88d7a09b35340b400433299392c3254263f6ef6
Gus <springheeljim@gmail.com>
2014-03-07 23:28:56 +0000

    adds new game widget creation to ui.  adds flex-shrink: 0 to top bar

--- js/client/widgets/LightsquareUi/LightsquareUi.js
+++ js/client/widgets/LightsquareUi/LightsquareUi.js
@@ -3,6 +3,7 @@ define(function(require) {
 	var html = require("file!./resources/lightsquare_ui.html");
 	var Template = require("lib/dom/Template");
 	var ChallengeList = require("widgets/ChallengeList/ChallengeList");
+	var Game = require("widgets/Game/Game");
 	
 	function LightSquareUi(parent, app) {
 		this._app = app;
@@ -13,6 +14,10 @@ define(function(require) {
 		this._template.new_challenge.addEventListener("click", (function() {
 			this._app.createChallenge({});
 		}).bind(this));
+		
+		this._app.NewGame.addHandler(this, function(data) {
+			var game = new Game(data.game, this._template.game);
+		});
 	}
 	
 	return LightSquareUi;
