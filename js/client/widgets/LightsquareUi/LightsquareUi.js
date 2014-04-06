c5caac07f49d0340ae8d5eeef2e5f4d2d6c75f65
Gus <springheeljim@gmail.com>
2014-04-06 22:44:22 +0100

    re-designs layout and implements routing system

--- js/client/widgets/LightsquareUi/LightsquareUi.js
+++ js/client/widgets/LightsquareUi/LightsquareUi.js
@@ -1,23 +1,62 @@
 define(function(require) {
 	require("css!./resources/lightsquare_ui.css");
 	var html = require("file!./resources/lightsquare_ui.html");
+	var linksHtml = require("file!./resources/links.html");
 	var Template = require("lib/dom/Template");
-	var ChallengeList = require("widgets/ChallengeList/ChallengeList");
-	var Game = require("widgets/Game/Game");
+	var Ractive = require("lib/dom/Ractive");
+	var Router = require("lib/Router");
 	
 	function LightSquareUi(parent, app) {
-		this._app = app;
-		this._template = new Template(html, parent);
+		var template = new Template(html, parent);
 		
-		this._challengeList = new ChallengeList(this._template.challenge_list, this._app);
-		
-		this._template.new_challenge.addEventListener("click", (function() {
-			this._app.createChallenge({});
-		}).bind(this));
-		
-		this._app.NewGame.addHandler(this, function(data) {
-			var game = new Game(data.game, this._template.game);
+		var links = new Ractive({
+			template: linksHtml,
+			el: template.nav,
+			data: {
+				links: [
+					{
+						href: "/",
+						label: "Home"
+					},
+					{
+						href: "/play",
+						label: "Play"
+					}
+				],
+				currentPath: window.location.pathname
+			}
 		});
+		
+		links.on("click", function(event) {
+			event.original.preventDefault();
+			router.loadPath(event.node.href);
+			links.set("currentPath", window.location.pathname);
+		});
+		
+		var router = new Router();
+		
+		router.addRoute("/", function() {
+			require(["./pages/Home/Home"], function(HomePage) {
+				template.main.innerHTML = "";
+				new HomePage(app, template.main);
+			});
+		});
+		
+		router.addRoute("/play", function() {
+			require(["./pages/Play/Play"], function(PlayPage) {
+				template.main.innerHTML = "";
+				new PlayPage(app, template.main);
+			});
+		});
+		
+		router.addRoute("/game/:id", function(params) {
+			require(["./pages/Game/Game"], function(GamePage) {
+				template.main.innerHTML = "";
+				new PlayPage(app, parseInt(params["id"]), template.main);
+			});
+		});
+		
+		router.loadPath();
 	}
 	
 	return LightSquareUi;
