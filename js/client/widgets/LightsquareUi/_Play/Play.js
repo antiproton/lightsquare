52fd11379c7d9f68bc5914b4ac7b7c7b5128cf90
Gus <springheeljim@gmail.com>
2014-04-07 23:13:46 +0100

    adds require for Play page css

--- js/client/widgets/LightsquareUi/_Play/Play.js
+++ js/client/widgets/LightsquareUi/_Play/Play.js
@@ -1,6 +1,7 @@
 define(function(require) {
 	var html = require("file!./resources/play.html");
 	var Template = require("lib/dom/Template");
+	require("css!./resources/play.css");
 	
 	function Play(app, parent) {
 		this._template = new Template(html, parent);
