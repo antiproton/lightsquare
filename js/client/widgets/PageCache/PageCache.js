e06bea70bbaaaa588241ae45fcb3ea67c0f7309d
Gus <springheeljim@gmail.com>
2014-04-12 16:14:33 +0100

    adds PageCache widget to act as a tabcontainer-like switcher for pages
    to avoid destroying/recreating pages if the user clicks back and forth loads

---
+++ js/client/widgets/PageCache/PageCache.js
@@ -0,0 +1,31 @@
+define(function(require) {
+	var create = require("lib/dom/create");
+	var style = require("lib/dom/style");
+	
+	function PageCache(parent) {
+		this._container = create("div", parent);
+		this._pages = {};
+	}
+	
+	PageCache.prototype.createPage = function(url) {
+		return this._pages[url] = create("div", this._container);
+	}
+	
+	PageCache.prototype.hasPage = function(url) {
+		return (url in this._pages);
+	}
+	
+	PageCache.prototype.showPage = function(url) {
+		for(var url in this._pages) {
+			style(this._pages[url], {
+				display: "none"
+			});
+		}
+		
+		style(this._pages[url], {
+			display: ""
+		});
+	}
+	
+	return PageCache;
+});
