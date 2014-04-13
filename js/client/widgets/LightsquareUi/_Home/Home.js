40f34785a2d6a2cb60df29aa69c70884faef70de
Gus <springheeljim@gmail.com>
2014-04-13 18:21:00 +0100

    adds board to _Home page

--- js/client/widgets/LightsquareUi/_Home/Home.js
+++ js/client/widgets/LightsquareUi/_Home/Home.js
@@ -2,9 +2,14 @@ define(function(require) {
 	var html = require("file!./resources/home.html");
 	var Template = require("lib/dom/Template");
 	require("css!./resources/home.css");
+	var Board = require("widgets/Board/Board");
 	
 	function Home(app, parent) {
 		this._template = new Template(html, parent);
+		
+		this._board = new Board(this._template.random_game);
+		this._board.setSquareSize(45);
+		this._board.setShowCoords(false);
 	}
 	
 	return Home;
