fcccdbc2d77021153219164c7423601e3fc6b9ca
Gus <springheeljim@gmail.com>
2014-04-14 19:52:13 +0100

    renames chessd to lightsquared

--- scripts/install-chessd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo ln -s /var/www/chess/scripts/chessd /etc/init.d/chessd
-sudo update-rc.d chessd defaults
--- scripts/uninstall-chessd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo rm /etc/init.d/chessd
-sudo update-rc.d chessd remove
