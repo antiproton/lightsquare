c07dbc90f394bfbc02b7869878a81812c10b8c86
Gus <springheeljim@gmail.com>
2014-04-18 00:02:41 +0100

    adds challenge list template and some update code to Lightsquare _HomePage

--- js/client/widgets/Lightsquare/_HomePage/HomePage.js
+++ js/client/widgets/Lightsquare/_HomePage/HomePage.js
@@ -1,8 +1,10 @@
 define(function(require) {
 	var html = require("file!./resources/home_page.html");
+	var challengeListHtml = require("file!./resources/challenge_list.html");
 	var Template = require("lib/dom/Template");
 	require("css!./resources/home_page.css");
 	var Board = require("widgets/Board/Board");
+	var Ractive = require("lib/dom/Ractive");
 	
 	function HomePage(app, parent) {
 		this._template = new Template(html, parent);
@@ -11,6 +13,20 @@ define(function(require) {
 		this._board.setSquareSize(60);
 		this._board.setShowCoords(false);
 		this._board.setSquareStyle(Board.squareStyles.GREEN);
+		
+		this._challengeList = new Ractive({
+			el: this._template.challenge_list,
+			template: challengeListHtml,
+			data: {
+				"games": []
+			}
+		});
+		
+		app.ChallengeExpired.addHandler(this, function(data) {
+			this._challengeList.set("games", this._challengeList.get("games").filter(function(challenge) {
+				return (challenge.id !== data.id);
+			}));
+		});
 	}
 	
 	return HomePage;
