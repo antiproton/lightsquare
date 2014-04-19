f556ddee1e09b39c1131af6adbb4c3001ffa8509
Gus <springheeljim@gmail.com>
2014-04-19 12:59:51 +0100

    renames LightsquareUi to Lightsquare in main

--- js/client/main.js
+++ js/client/main.js
@@ -2,11 +2,11 @@ define(function(require) {
 	require("lib/dom/ready!");
 	var Server = require("lib/websocket-client/Server");
 	var Application = require("./Application");
-	var LightsquareUi = require("./widgets/Lightsquare/Lightsquare");
-	
+	var Lightsquare = require("./widgets/Lightsquare/Lightsquare");
+
 	var server = new Server("ws://" + window.location.hostname + ":8080");
 	var app = new Application(server);
-	var ui = new LightsquareUi(app, document.body);
+	var ui = new Lightsquare(app, document.body);
 	
 	server.connect();
 });
