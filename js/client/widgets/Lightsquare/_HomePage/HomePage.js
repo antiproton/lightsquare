38810dba2319941bb5d039a420b8af6594500d65
Gus <springheeljim@gmail.com>
2014-04-22 22:06:48 +0100

    adds dashboard (Home) and Players (list of online players) sections to HomePage
    Players list not implemented yet.  Dashboard will be intro and login/registration form
    if user isn't logged in, or newsfeed/stats etc if logged in.

--- js/client/widgets/Lightsquare/_HomePage/HomePage.js
+++ js/client/widgets/Lightsquare/_HomePage/HomePage.js
@@ -1,13 +1,29 @@
 define(function(require) {
 	var html = require("file!./resources/home_page.html");
 	var challengeListHtml = require("file!./resources/challenge_list.html");
+	var dashboardHtml = require("file!./resources/dashboard.html");
 	var Template = require("lib/dom/Template");
 	require("css!./resources/home_page.css");
 	var Board = require("widgets/Board/Board");
 	var Ractive = require("lib/dom/Ractive");
 	
 	function HomePage(app, parent) {
+		this._app = app;
 		this._template = new Template(html, parent);
+		
+		this._dashboard = new Ractive({
+			el: this._template.dashboard,
+			template: dashboardHtml,
+			data: {
+				
+			}
+		});
+		
+		this._dashboard.on("login", (function(event) {
+			event.original.preventDefault();
+			
+			
+		}).bind(this));
 		
 		this._board = new Board(this._template.random_game);
 		this._board.setSquareSize(60);
@@ -17,7 +33,7 @@ define(function(require) {
 		this._template.quick_challenge_form.addEventListener("submit", (function(event) {
 			event.preventDefault();
 			
-			app.createChallenge({
+			this._app.createChallenge({
 				initialTime: this._template.quick_challenge_initial_time.value,
 				timeIncrement: this._template.quick_challenge_increment.value,
 				acceptRatingMin: this._template.quick_challenge_rating_min.value,
@@ -33,15 +49,15 @@ define(function(require) {
 			}
 		});
 		
-		this._challengeList.on("accept", function(event, id) {
-			app.acceptChallenge(id);
-		});
+		this._challengeList.on("accept", (function(event, id) {
+			this._app.acceptChallenge(id);
+		}).bind(this));
 		
-		app.NewChallenge.addHandler(this, function(data) {
+		this._app.NewChallenge.addHandler(this, function(data) {
 			this._challengeList.get("challenges").push(data.challenge);
 		});
 		
-		app.ChallengeExpired.addHandler(this, function(data) {
+		this._app.ChallengeExpired.addHandler(this, function(data) {
 			this._challengeList.set("challenges", this._challengeList.get("challenges").filter(function(challenge) {
 				return (challenge.id !== data.id);
 			}));
