86eb4b0e321bd8aaf80864a1ed0b3dad342e8f59
Gus <springheeljim@gmail.com>
2014-05-04 17:39:11 +0100

    adds basic DOM structure and a Board widget to GamePage template

--- js/client/widgets/Lightsquare/_GamePage/GamePage.js
+++ js/client/widgets/Lightsquare/_GamePage/GamePage.js
@@ -2,11 +2,14 @@ define(function(require) {
 	var html = require("file!./resources/game_page.html");
 	require("css!./resources/game_page.css");
 	var Template = require("lib/dom/Template");
+	var Board = require("widgets/Board/Board");
 	
 	function GamePage(game, user, parent) {
 		this._template = new Template(html, parent);
 		this._game = game;
 		this._user = user;
+		
+		this._board = new Board(this._template.board);
 	}
 	
 	return GamePage;
