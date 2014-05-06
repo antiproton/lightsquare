81e0d7254d4981946e87105b241b00bd8b00b102
Gus <springheeljim@gmail.com>
2014-05-06 20:31:00 +0100

    refactors client Clock to use internal chess Clock and only adjust time when retrieving the current player's remaining time

--- js/client/Clock.js
+++ js/client/Clock.js
@@ -2,52 +2,46 @@ define(function(require) {
 	var time = require("lib/time");
 	var Event = require("lib/Event");
 	var Time = require("chess/Time");
-	var Colour = require("chess/Colour");
+	var ChessClock = require("chess/Clock");
 	
-	function Clock(server, game) {
+	function Clock(server, game, timingStyle) {
 		this._server = server;
-		this._timingStyle = game.getTimingStyle();
-		
-		this._moves = game.getHistory();
-		
-		game.Move.addHandler(this, function(data) {
-			this._moves.push(data.move);
-		});
-		
-		this._timeLeft = {};
-		this._timeLeft[Colour.white] = Time.fromMilliseconds(this._timingStyle.initialTime);
-		this._timeLeft[Colour.black] = Time.fromMilliseconds(this._timingStyle.initialTime);
-		
+		this._clock = new ChessClock(game, timingStyle);
 		this._serverTimeDifference = 0;
-		this._getLatencyAndTimeDifference();
-		this._calculateTime();
+		this._estimateTimeDifference();
 	}
 	
 	Clock.prototype.getTimeLeft = function(colour) {
-		return Time.fromMilliseconds(this._timeLeft[colour]);
+		var timeLeft = this._clock.getTimeLeft(colour);
+		
+		if(colour === this._game.getPosition.getActiveColour()) {
+			timeLeft += this._serverTimeDifference;
+		}
+		
+		return Time.fromMilliseconds(timeLeft);
 	}
 	
-	Clock.prototype._getLatencyAndTimeDifference = function() {
+	Clock.prototype._estimateTimeDifference = function() {
 		var numberOfRequestsToSend = 3;
 		var timeBetweenRequests = 500;
 		var timeLastRequestSent;
-		var latencies = [];
+		var recordedLatencies = [];
 		
 		this._server.subscribe("/time", (function(serverTime) {
 			var now = time();
 			var latency = now - timeLastRequestSent;
 			
-			latencies.push(latency);
+			recordedLatencies.push(latency);
 			
-			var averageLatency = latencies.reduce(function(total, current) {
+			var averageLatency = recordedLatencies.reduce(function(total, current) {
 				return total + current;
-			}, 0) / latencies.length;
+			}, 0) / recordedLatencies.length;
 			
 			var estimatedServerTime = serverTime + Math.round(averageLatency / 2);
 			 
 			this._serverTimeDifference = estimatedServerTime - now;
 			
-			if(latencies.length < numberOfRequestsToSend) {
+			if(recordedLatencies.length < numberOfRequestsToSend) {
 				setTimeout(requestTime, timeBetweenRequests - latency);
 			}
 		}).bind(this));
@@ -59,20 +53,6 @@ define(function(require) {
 		}).bind(this);
 		
 		requestTime();
-	}
-	
-	Clock.prototype._calculateTime = function() {
-		/*
-		TODO copy this functionality from echochess code
-		
-		..
-		
-		setTimeout(do it again)
-		*/
-	}
-	
-	Clock.prototype._getEstimatedServerTime = function() {
-		return time() + this._serverTimeDifference;
 	}
 	
 	return Clock;
