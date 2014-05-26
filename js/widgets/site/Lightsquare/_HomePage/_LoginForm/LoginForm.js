978c86364b311982847165267e1b1bb086bf9c64
Gus <springheeljim@gmail.com>
2014-05-26 14:05:43 +0100

    adds error messages to LoginForm

--- js/widgets/site/Lightsquare/_HomePage/_LoginForm/LoginForm.js
+++ js/widgets/site/Lightsquare/_HomePage/_LoginForm/LoginForm.js
@@ -10,12 +10,18 @@ define(function(require) {
 			el: parent,
 			template: html,
 			data: {
-				register: false
+				register: false,
+				error: ""
 			}
 		});
 		
-		this._template.on("submit", (function(event, username, password) {
+		this._template.on("submit", (function(event) {
 			event.original.preventDefault();
+			
+			this._clearError();
+			
+			var username = this._template.get("username").toString();
+			var password = this._template.get("password").toString();
 			
 			if(this._template.get("register")) {
 				this._user.register(username, password);
@@ -26,13 +32,44 @@ define(function(require) {
 			}
 		}).bind(this));
 		
-		this._template.on("login", (function(event, username, password) {
+		this._template.on("login", (function(event) {
 			this._template.set("register", false);
+			this._clearError();
 		}).bind(this));
 		
-		this._template.on("register", (function(event, username, password) {
+		this._template.on("register", (function(event) {
 			this._template.set("register", true);
+			this._clearError();
 		}).bind(this));
+		
+		this._user.LoggedIn.addHandler(this, function() {
+			this._clearForm();
+		});
+		
+		this._user.LoginFailed.addHandler(this, function(data) {
+			this._setError(data.reason);
+		});
+		
+		this._user.Registered.addHandler(this, function() {
+			this._clearForm();
+		});
+		
+		this._user.RegistrationFailed.addHandler(this, function(data) {
+			this._setError(data.reason);
+		});
+	}
+	
+	LoginForm.prototype._clearForm = function() {
+		this._template.set("username", "");
+		this._template.set("password", "");
+	}
+	
+	LoginForm.prototype._clearError = function() {
+		this._template.set("error", "");
+	}
+	
+	LoginForm.prototype._setError = function(message) {
+		this._template.set("error", message);
 	}
 	
 	return LoginForm;
