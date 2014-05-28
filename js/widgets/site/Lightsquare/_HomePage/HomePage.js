94acf74c6d49c8c04a376a888ff7863b0bb9fcfc
Gus <springheeljim@gmail.com>
2014-05-28 22:36:02 +0100

    hides login form if user is logged in on HomePage

--- js/widgets/site/Lightsquare/_HomePage/HomePage.js
+++ js/widgets/site/Lightsquare/_HomePage/HomePage.js
@@ -12,6 +12,7 @@ define(function(require) {
 		this._template = new Template(html, parent);
 		
 		this._setupLoginForm();
+		this._handleUserEvents();
 		this._setupCreateChallengeForm();
 		this._setupChallengeGraph();
 	}
@@ -26,6 +27,24 @@ define(function(require) {
 	
 	HomePage.prototype._setupChallengeGraph = function() {
 		this._challengeGraph = new ChallengeGraph(this._app, this._user, this._template.challenge_graph);
+	}
+	
+	HomePage.prototype._handleUserEvents = function() {
+		this._user.LoggedIn.addHandler(this, function() {
+			this._updateLoginDependentUiElements();
+		});
+		
+		this._user.LoggedOut.addHandler(this, function() {
+			this._updateLoginDependentUiElements();
+		});
+		
+		this._user.HasIdentity.addHandler(this, function() {
+			this._updateLoginDependentUiElements();
+		});
+	}
+	
+	HomePage.prototype._updateLoginDependentUiElements = function() {
+		this._template.login_form.style.display = (this._user.isLoggedIn() ? "none" : "");
 	}
 	
 	return HomePage;
