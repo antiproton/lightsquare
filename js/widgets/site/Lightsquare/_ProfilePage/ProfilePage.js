364c1a6d10ac32f74f12c7d425590c6336873061
Gus <springheeljim@gmail.com>
2014-05-29 22:02:24 +0100

    adds empty template and css for ProfilePage

--- js/widgets/site/Lightsquare/_ProfilePage/ProfilePage.js
+++ js/widgets/site/Lightsquare/_ProfilePage/ProfilePage.js
@@ -1,6 +1,10 @@
 define(function(require) {
-	function ProfilePage(user, parent) {
-		
+	require("css!./profile_page.css");
+	var html = require("file!./profile_page.html");
+	var Template = require("lib/dom/Template");
+	
+	function ProfilePage(parent) {
+		this._template = new Template(html, parent);
 	}
 	
 	return ProfilePage;
