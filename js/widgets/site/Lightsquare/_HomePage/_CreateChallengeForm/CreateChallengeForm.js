9de5abc2822db6614954dc3355217a35de1afbfd
Gus <springheeljim@gmail.com>
2014-05-30 22:10:06 +0100

    adds challenge cancel functionality to CreateChallengeForm
    the button currently goes transparent and changes to 'Cancel', but it needs
    animating with a draining-out effect the length of the challenge timeout.
    
    this turns out not to work very well with css animations; the browser doesn't
    seem to smooth out the transitions between the keyframes so you would need
    hundreds to make the animation smooth.  should be easy enough to come up
    with a js solution.

--- js/widgets/site/Lightsquare/_HomePage/_CreateChallengeForm/CreateChallengeForm.js
+++ js/widgets/site/Lightsquare/_HomePage/_CreateChallengeForm/CreateChallengeForm.js
@@ -1,12 +1,15 @@
 define(function(require) {
+	require("css!./create_challenge_form.css");
 	var html = require("file!./create_challenge_form.html");
 	var Ractive = require("lib/dom/Ractive");
+	var jsonChessConstants = require("jsonchess/constants");
 	
 	function CreateChallengeForm(user, parent) {
 		var template = new Ractive({
 			el: parent,
 			template: html,
 			data: {
+				waiting: false,
 				initialTime: "10m",
 				timeIncrement: "5",
 				ratingMin: "-100",
@@ -14,15 +17,31 @@ define(function(require) {
 			}
 		});
 		
-		template.on("submit", function(event) {
+		var waitingTimer = null;
+		
+		template.on("create_or_cancel", function(event) {
 			event.original.preventDefault();
 			
-			user.createChallenge({
-				initialTime: template.get("initialTime").toString(),
-				timeIncrement: template.get("timeIncrement").toString(),
-				acceptRatingMin: template.get("ratingMin").toString(),
-				acceptRatingMax: template.get("ratingMax").toString()
-			});
+			if(template.get("waiting")) {
+				user.cancelChallenge();
+				template.set("waiting", false);
+				clearTimeout(waitingTimer);
+			}
+			
+			else {
+				user.createChallenge({
+					initialTime: template.get("initialTime").toString(),
+					timeIncrement: template.get("timeIncrement").toString(),
+					acceptRatingMin: template.get("ratingMin").toString(),
+					acceptRatingMax: template.get("ratingMax").toString()
+				});
+				
+				template.set("waiting", true);
+				
+				waitingTimer = setTimeout(function() {
+					template.set("waiting", false);
+				}, jsonChessConstants.CHALLENGE_TIMEOUT);
+			}
 		});
 	}
 	
