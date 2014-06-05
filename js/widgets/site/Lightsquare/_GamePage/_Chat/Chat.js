0da363fc165caa52ef4e672501a96af37489f868
Gus <gus@hogg-blake.co.uk>
2014-06-05 21:35:13 +0100

    implements auto-scrolling on new messages in Chat

--- js/widgets/site/Lightsquare/_GamePage/_Chat/Chat.js
+++ js/widgets/site/Lightsquare/_GamePage/_Chat/Chat.js
@@ -5,12 +5,38 @@ define(function(require) {
 	
 	function Chat(game, parent) {
 		this._game = game;
+		this._scrollOnNewMessages = true;
 		
 		this._template = new Ractive({
 			el: parent,
 			template: html,
 			data: {
+				message: "",
 				messages: []
+			}
+		});
+		
+		var history = this._template.nodes.chat_history;
+		
+		history.addEventListener("scroll", (function() {
+			this._scrollOnNewMessages = (history.scrollHeight - history.scrollTop === history.clientHeight);
+		}).bind(this));
+		
+		this._template.on("send", (function(event) {
+			event.original.preventDefault();
+			
+			this._game.sendChatMessage(this._template.get("message"));
+			this._template.set("message", "");
+		}).bind(this));
+		
+		this._game.ChatMessageReceived.addHandler(this, function(message) {
+			this._template.get("messages").push({
+				from: message.from,
+				body: message.body
+			});
+			
+			if(this._scrollOnNewMessages) {
+				history.scrollTop = history.scrollHeight;
 			}
 		});
 	}
