10d4a9751423b4c09cb3900933ba33192a56303e
Gus <gus@hogg-blake.co.uk>
2014-06-05 14:12:05 +0100

    adds text, styles and loading gif to LoadingIndicator

--- js/widgets/site/LoadingIndicator/LoadingIndicator.js
+++ js/widgets/site/LoadingIndicator/LoadingIndicator.js
@@ -1,8 +1,23 @@
 define(function(require) {
+	require("css!./loadingIndicator.css");
+	var html = require("file!./loadingIndicator.html");
 	var Ractive = require("lib/dom/Ractive");
 	
 	function LoadingIndicator(parent, secondsToWait) {
+		this._template = new Ractive({
+			el: parent,
+			template: html,
+			data: {
+				getAbsolutePath: function(path) {
+					return require.toUrl(path);
+				},
+				troubleConnecting: false
+			}
+		});
 		
+		setTimeout((function() {
+			this._template.set("troubleConnecting", true);
+		}).bind(this), secondsToWait * 1000);
 	}
 	
 	return LoadingIndicator;
