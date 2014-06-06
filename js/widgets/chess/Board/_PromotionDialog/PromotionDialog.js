c6a5a2bb55227e33d7b21c0b5763b9be46a68e0a
Gus <gus@hogg-blake.co.uk>
2014-06-06 21:11:13 +0100

    finishes implementing PromotionDialog widget

--- js/widgets/chess/Board/_PromotionDialog/PromotionDialog.js
+++ js/widgets/chess/Board/_PromotionDialog/PromotionDialog.js
@@ -1,26 +1,51 @@
 define(function(require) {
 	require("css!./promotionDialog.css");
 	var html = require("file!./promotionDialog.html");
-	var Ractive = require("lib/dom/Ractive");
+	var Event = require("lib/Event");
+	var create = require("lib/dom/create");
+	var Template = require("lib/dom/Template");
 	var PieceType = require("chess/PieceType");
-	var Event = require("lib/Event");;
+	var ChessPiece = require("chess/Piece");
+	var Piece = require("widgets/chess/Piece/Piece");
+	var Colour = require("chess/Colour");
 	
-	function PromotionDialog(parent) {
+	function PromotionDialog(pieceSize, parent) {
 		this.PieceSelected = new Event(this);
 		
-		this._template = new Ractive({
-			el: parent,
-			template: html,
-			data: {
-				pieceTypes: [PieceType.queen, PieceType.rook, PieceType.bishop, PieceType.knight]
-			}
-		});
+		this._template = new Template(html, parent);
+		this._pieces = [];
 		
-		this._template.on("select", (function(event, type) {
-			this.PieceSelected.fire({
-				type: type
-			});
+		[
+			PieceType.queen,
+			PieceType.rook,
+			PieceType.bishop,
+			PieceType.knight
+		].forEach((function(type) {
+			var pieceButton = create("div", this._template.pieces);
+			var piece = new Piece(pieceButton, pieceSize);
+			
+			piece.setPiece(ChessPiece.get(type, Colour.white));
+			
+			this._pieces.push(piece);
+			
+			pieceButton.addEventListener("click", (function() {
+				this.PieceSelected.fire({
+					type: type
+				});
+			}).bind(this));
 		}).bind(this));
+	}
+	
+	PromotionDialog.prototype.setPieceStyle = function(pieceStyle) {
+		this._pieces.forEach(function(piece) {
+			piece.setStyle(pieceStyle);
+		});
+	}
+	
+	PromotionDialog.prototype.setColour = function(colour) {
+		this._pieces.forEach(function(piece) {
+			piece.setPiece(ChessPiece.get(piece.getPiece().type, colour));
+		});
 	}
 	
 	return PromotionDialog;
