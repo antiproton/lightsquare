f84eb7036f37da9d392b00de5a3ccab5a5fb2375
Gus <gus@hogg-blake.co.uk>
2014-06-24 20:46:07 +0100

    converts HomePage to use Ractive instead of Template

--- js/widgets/site/Lightsquare/_HomePage/HomePage.js
+++ js/widgets/site/Lightsquare/_HomePage/HomePage.js
@@ -1,7 +1,7 @@
 define(function(require) {
 	require("css!./home_page.css");
 	var html = require("file!./home_page.html");
-	var Template = require("lib/dom/Template");
+	var Ractive = require("lib/dom/Ractive");
 	var CreateChallengeForm = require("./_CreateChallengeForm/CreateChallengeForm");
 	var ChallengeGraph = require("./_ChallengeGraph/ChallengeGraph");
 	var LoginForm = require("./_LoginForm/LoginForm");
@@ -9,28 +9,27 @@ define(function(require) {
 	function HomePage(app, user, parent) {
 		this._app = app;
 		this._user = user;
-		this._template = new Template(html, parent);
 		
-		this._setupLoginForm();
+		this._setupTemplate(parent);
 		this._handleUserEvents();
-		this._setupCreateChallengeForm();
-		this._setupChallengeGraph();
-		
+
 		this._user.getDetails().then((function() {
 			this._updateUserDependentElements();
 		}).bind(this));
 	}
 	
-	HomePage.prototype._setupLoginForm = function() {
-		this._loginForm = new LoginForm(this._user, this._template.login_form);
-	}
-	
-	HomePage.prototype._setupCreateChallengeForm = function() {
-		this._createChallengeForm = new CreateChallengeForm(this._user, this._template.create_challenge);
-	}
-	
-	HomePage.prototype._setupChallengeGraph = function() {
-		this._challengeGraph = new ChallengeGraph(this._app, this._user, this._template.challenge_graph);
+	HomePage.prototype._setupTemplate = function(parent) {
+		this._template = new Ractive({
+			el: parent,
+			template: html,
+			data: {
+				userIsLoggedIn: this._user.isLoggedIn()
+			}
+		});
+		
+		this._loginForm = new LoginForm(this._user, this._template.nodes.login_form);
+		this._createChallengeForm = new CreateChallengeForm(this._user, this._template.nodes.create_challenge);
+		this._challengeGraph = new ChallengeGraph(this._app, this._user, this._template.nodes.challenge_graph);
 	}
 	
 	HomePage.prototype._handleUserEvents = function() {
@@ -44,7 +43,7 @@ define(function(require) {
 	}
 	
 	HomePage.prototype._updateUserDependentElements = function() {
-		this._template.login_form.style.display = (this._user.isLoggedIn() ? "none" : "");
+		this._template.set("userIsLoggedIn", this._user.isLoggedIn());
 	}
 	
 	return HomePage;
