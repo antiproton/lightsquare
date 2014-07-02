b55d53366aa70c487608a66eaa9ac0af0d0bf22f
Gus <gus@hogg-blake.co.uk>
2014-07-02 18:24:08 +0100

    removes GameOver handler from Game - only receiving /game_over should call _gameOver
    otherwise Game fires GameOver twice

--- js/Game.js
+++ js/Game.js
@@ -58,10 +58,6 @@ define(function(require) {
 		this._clock = new Clock(this._server, this, this._timingStyle);
 		
 		if(this._isInProgress) {
-			this._game.GameOver.addHandler(this, function(result) {
-				this._gameOver(result);
-			});
-			
 			this._requestLatestMoves();
 			this._clockTick();
 		}
