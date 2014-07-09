de76a28b3356e08f35ed7e01cb57a0f0cbf44c63
Gus <gus@hogg-blake.co.uk>
2014-07-09 23:06:40 +0100

    adds boards to game backups in GameBackupList

--- js/widgets/site/Lightsquare/_HomePage/_GameBackupList/GameBackupList.js
+++ js/widgets/site/Lightsquare/_HomePage/_GameBackupList/GameBackupList.js
@@ -1,21 +1,40 @@
 define(function(require) {
 	require("css!./game_backup_list.css");
 	var html = require("file!./game_backup_list.html");
+	var Board = require("widgets/chess/Board/Board");
+	var Position = require("chess/Position");
 	
 	function GameBackupList(user, parent) {
 		this._user = user;
+		
+		this._boards = {};
 		
 		this._template = new Ractive({
 			el: parent,
 			template: html,
 			data: {
-				gameBackups: this._user.getGameBackups()
+				gameBackups: {},
 			}
 		});
 	}
 	
 	GameBackupList.prototype.refresh = function() {
-		this._template.set("gameBackups", this._user.getGameBackups());
+		var backups = this._user.getGameBackups();
+		
+		this._boards = {};
+		this._template.set("gameBackups", {});
+		
+		for(var id in backups) {
+			
+			this._template.set("gameBackups." + id, backups[id]);
+			
+			var board  = new Board(this._template.nodes["board_" + id]);
+			
+			board.setSquareSize(20);
+			board.setShowCoords(false);
+			this._boards[id] = board;
+			
+		}
 	}
 	
 	return GameBackupList;
