477bb51cc7f3a27981d1d9081507e2015adcc60e
Gus <gus@hogg-blake.co.uk>
2014-07-15 17:06:23 +0100

    refactors GameBackupList to use new RestorationRequest design and adds GameRestored event

--- js/widgets/site/Lightsquare/_HomePage/_GameBackupList/GameBackupList.js
+++ js/widgets/site/Lightsquare/_HomePage/_GameBackupList/GameBackupList.js
@@ -4,16 +4,30 @@ define(function(require) {
 	var Board = require("widgets/chess/Board/Board");
 	var Position = require("chess/Position");
 	var Colour = require("chess/Colour");
+	var Event = require("lib/Event");
 	
 	function GameBackupList(user, parent) {
 		this._user = user;
-		
 		this._boards = {};
+		this._setupTemplate(parent);
+		this._restorationRequests = {};
 		
+		this.GameRestored = new Event(this);
+		
+		this._user.getPendingRestorations().then((function(ids) {
+			ids.forEach((function(id) {
+				this._template.set("restorationRequestSubmitted." + id, true)
+			}).bind(this));
+		}).bind(this));
+	}
+	
+	GameBackupList.prototype._setupTemplate = function(parent) {
 		this._template = new Ractive({
 			el: parent,
 			template: html,
 			data: {
+				selectedMove: {},
+				restorationRequestSubmitted: {},
 				gameBackups: {},
 				formatDate: function(time) {
 					return new Date(time).toLocaleString();
@@ -22,30 +36,36 @@ define(function(require) {
 		});
 		
 		this._template.on("select_move", (function(event, id) {
-			this._boards[id].setBoardArray(new Position(event.context.resultingFen).getBoardArray());
-			this._template.set("selectedMove." + id, event.context);
+			var moveDetails = event.context;
+			
+			this._boards[id].setBoardArray(new Position(moveDetails.resultingFen).getBoardArray());
+			this._template.set("selectedMove." + id, moveDetails);
 		}).bind(this));
 		
 		this._template.on("restore_or_cancel", (function(event, id) {
 			var backup = event.context;
 			
-			if(backup.restorationRequestSubmitted) {
+			if(this._template.get("restorationRequestSubmitted." + id)) {
+				this._createRestorationRequest(backup).cancel();
+				
 				this._user.cancelGameRestoration(id).then((function() {
-					this._template.set("gameBackups." + id + ".restorationRequestSubmitted", false);
+					this._template.set("restorationRequestSubmitted." + id, false);
 				}).bind(this));
 			}
 			
 			else {
-				var request = this._user.requestGameRestoration(backup);
+				var request = this._createRestorationRequest(backup).submit();
 				
 				request.onProgress((function() {
-					this._template.set("gameBackups." + id + ".restorationRequestSubmitted", true);
+					this._template.set("restorationRequestSubmitted." + id, true);
 				}).bind(this));
 				
 				request.then((function() {
 					this.refresh();
 				}).bind(this), (function(error) {
 					this._template.set("error." + id, error);
+				}).bind(this), (function() {
+					delete this._restorationRequests[id];
 				}).bind(this));
 			}
 		}).bind(this));
@@ -75,6 +95,23 @@ define(function(require) {
 			board.setViewingAs(Colour.fromFenString(backup.playingAs));
 			
 			this._boards[id] = board;
+		}
+	}
+	
+	GameBackupList.prototype._createRestorationRequest = function(backup) {
+		var id = backup.gameDetails.id;
+		var request;
+		
+		if(id in this._restorationRequests) {
+			request = this._restorationRequests[id];
+		}
+		
+		else {
+			request = this._restorationRequests[id] = this._user.createRestorationRequest(backup);
+			
+			request.GameRestored.addHandler(this, function() {
+				this.GameRestored.fire();
+			});
 		}
 	}
 	
