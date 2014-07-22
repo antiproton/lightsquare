5b90cfdc6cb2f1451ed38877653e58a4932fa750
Gus <gus@hogg-blake.co.uk>
2014-07-22 10:43:10 +0100

    adds a timer to clear the error on LoginForm after 10 seconds

--- js/widgets/site/Lightsquare/_HomePage/_LoginForm/LoginForm.js
+++ js/widgets/site/Lightsquare/_HomePage/_LoginForm/LoginForm.js
@@ -16,10 +16,13 @@ define(function(require) {
 			}
 		});
 		
+		this._clearErrorTimer = null;
+		
 		this._template.on("submit", (function(event) {
 			event.original.preventDefault();
 			
 			this._clearError();
+			this._clearClearErrorTimer();
 			
 			var username = this._template.get("username") + "";
 			var password = this._template.get("password") + "";
@@ -43,6 +46,22 @@ define(function(require) {
 	
 	LoginForm.prototype._setError = function(message) {
 		this._template.set("error", message);
+		this._setClearErrorTimer();
+	}
+	
+	LoginForm.prototype._setClearErrorTimer = function() {
+		this._clearErrorTimer = setTimeout((function() {
+			this._clearError();
+			this._clearErrorTimer = null;
+		}).bind(this), 10 * 1000);
+	}
+	
+	LoginForm.prototype._clearClearErrorTimer = function() {
+		if(this._clearErrorTimer !== null) {
+			clearTimeout(this._clearErrorTimer);
+			
+			this._clearErrorTimer = null;
+		}
 	}
 	
 	return LoginForm;
