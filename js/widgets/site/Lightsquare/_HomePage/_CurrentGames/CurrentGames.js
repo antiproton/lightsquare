45e5be0c825e3b546d8389ca943f7d43f088934d
Gus <gus@hogg-blake.co.uk>
2014-07-23 19:27:54 +0100

    updates CurrentGames to use the new simpler update events from RandomGames

--- js/widgets/site/Lightsquare/_HomePage/_CurrentGames/CurrentGames.js
+++ js/widgets/site/Lightsquare/_HomePage/_CurrentGames/CurrentGames.js
@@ -38,16 +38,8 @@ define(function(require) {
 			}
 		});
 		
-		this._gamesList.NewGame.addHandler(function(gameDetails) {
-			this._addGame(gameDetails);
-		}, this);
-		
-		this._gamesList.Move.addHandler(function(data) {
-			var id = data.gameId;
-			var move = Move.fromJSON(data.move);
-			
-			this._template.set("games." + id + ".board", move.getPositionAfter().getBoardArray());
-			this._template.set("games." + id + ".last_move", move);
+		this._gamesList.Update.addHandler(function(gameDetails) {
+			this._updateGame(gameDetails);
 		}, this);
 		
 		this._gamesList.GameOver.addHandler(function(id) {
@@ -57,32 +49,17 @@ define(function(require) {
 	
 	CurrentGames.prototype.startUpdating = function() {
 		this._gamesList.startUpdating();
-		
-		this._gamesList.getGames().then((function(games) {
-			games.forEach((function(gameDetails) {
-				this._addGame(gameDetails);
-			}).bind(this));
-		}).bind(this));
 	}
 	
 	CurrentGames.prototype.stopUpdating = function() {
 		this._gamesList.stopUpdating();
 	}
 	
-	CurrentGames.prototype._addGame = function(gameDetails) {
+	CurrentGames.prototype._updateGame = function(gameDetails) {
 		var id = gameDetails.id;
-		var position;
 		
-		if(gameDetails.history.length > 0) {
-			position = Move.fromJSON(gameDetails.history[gameDetails.history.length - 1]).getPositionAfter();
-		}
-		
-		else {
-			position = new Position();
-		}
-		
-		this._template.set("games." + id + ".board", position.getBoardArray());
-		this._template.set("games." + id + ".last_move", null);
+		this._template.set("games." + id + ".board", new Position(gameDetails.fen).getBoardArray());
+		this._template.set("games." + id + ".lastMove", gameDetails.lastMove);
 	}
 	
 	return CurrentGames;
