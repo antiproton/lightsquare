300461db26b86114db8ccb3ec9b9b58290e48e22
Gus <gus@hogg-blake.co.uk>
2014-07-23 19:26:38 +0100

    updates RandomGames to match the new simpler message system - the only events are Update and GameOver now

--- js/RandomGames.js
+++ js/RandomGames.js
@@ -1,15 +1,11 @@
 define(function(require) {
 	var Event = require("lib/Event");
-	var Promisor = require("lib/Promisor");
 	
 	function RandomGames(server) {
 		this._server = server;
-		this._games = {};
-		this._promisor = new Promisor(this);
 		
-		this.NewGame = new Event();
+		this.Update = new Event();
 		this.GameOver = new Event();
-		this.Move = new Event();
 		
 		this._subscribeToServerMessages();
 	}
@@ -20,45 +16,15 @@ define(function(require) {
 	
 	RandomGames.prototype.stopUpdating = function() {
 		this._server.send("/random_games/unsubscribe");
-		this._promisor.remove("/games");
-	}
-	
-	RandomGames.prototype.getGames = function() {
-		return this._promisor.getPersistent("/games");
 	}
 	
 	RandomGames.prototype._subscribeToServerMessages = function() {
-		this._server.subscribe("/random_game/new", (function(gameDetails) {
-			this._games[gameDetails.id] = gameDetails;
-			this.NewGame.fire(gameDetails);
+		this._server.subscribe("/random_game", (function(gameDetails) {
+			this.Update.fire(gameDetails);
 		}).bind(this));
 		
 		this._server.subscribe("/random_game/game_over", (function(id) {
-			delete this._games[id];
-			
 			this.GameOver.fire(id);
-		}).bind(this));
-		
-		this._server.subscribe("/random_game/move", (function(data) {
-			var id = data.gameId;
-			var move = data.move;
-			
-			if(id in this._games) {
-				this._games[id].history.push(move);
-				
-				this.Move.fire({
-					gameId: id,
-					move: move
-				});
-			}
-		}).bind(this));
-		
-		this._server.subscribe("/random_games", (function(games) {
-			games.forEach((function(gameDetails) {
-				this._games[gameDetails.id] = gameDetails;
-			}).bind(this));
-			
-			this._promisor.resolve("/games", games);
 		}).bind(this));
 	}
 	
