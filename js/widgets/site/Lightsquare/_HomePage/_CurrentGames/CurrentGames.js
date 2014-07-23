46a1cdb6bb36036314c47253612b52afd3989b72
Gus <gus@hogg-blake.co.uk>
2014-07-23 12:43:28 +0100

    adds to CurrentGames code and template.  adds sprite for pieces.

--- js/widgets/site/Lightsquare/_HomePage/_CurrentGames/CurrentGames.js
+++ js/widgets/site/Lightsquare/_HomePage/_CurrentGames/CurrentGames.js
@@ -3,6 +3,7 @@ define(function(require) {
 	var html = require("file!./current_games.html");
 	var Ractive = require("lib/dom/Ractive");
 	var Move = require("jsonchess/Move");
+	var Position = require("chess/Position");
 	
 	function CurrentGames(gamesList, parent) {
 		this._gamesList = gamesList;
@@ -11,30 +12,33 @@ define(function(require) {
 			el: parent,
 			template: html,
 			data: {
+				squareSize: 30,
+				pieceUrl: require.toUrl("./piece_sprite.png"),
+				getSquareTopOffset: function(squareNo) {
+					
+				},
+				getSquareLeftOffset: function(squareNo) {
+					
+				},
+				getSquareColour: function(squareNo) {
+					
+				},
+				getPieceOffset: function(piece) {
+					
+				},
 				games: {}
 			}
 		});
 		
 		this._gamesList.NewGame.addHandler(function(gameDetails) {
-			var position;
-			
-			if(gameDetails.history.length > 0) {
-				position = Move.fromJSON(gameDetails.history[gameDetails.history.length - 1]).getPosition();
-			}
-			
-			else {
-				position = new Position();
-			}
-			
-			this._template.set("games." + id + ".board", position.getBoardArray());
-			this._template.set("games." + id + ".last_move", null);
+			this._addGame(gameDetails);
 		}, this);
 		
 		this._gamesList.Move.addHandler(function(data) {
 			var id = data.gameId;
 			var move = Move.fromJSON(data.move);
 			
-			this._template.set("games." + id + ".board", move.getPosition().getBoardArray());
+			this._template.set("games." + id + ".board", move.getPositionAfter().getBoardArray());
 			this._template.set("games." + id + ".last_move", move);
 		}, this);
 		
@@ -47,12 +51,30 @@ define(function(require) {
 		this._gamesList.startUpdating();
 		
 		this._gamesList.getGames().then((function(games) {
-			this._template.set("games", games);
+			games.forEach((function(gameDetails) {
+				this._addGame(gameDetails);
+			}).bind(this));
 		}).bind(this));
 	}
 	
 	CurrentGames.prototype.stopUpdating = function() {
 		this._gamesList.stopUpdating();
+	}
+	
+	CurrentGames.prototype._addGame = function(gameDetails) {
+		var id = gameDetails.id;
+		var position;
+		
+		if(gameDetails.history.length > 0) {
+			position = Move.fromJSON(gameDetails.history[gameDetails.history.length - 1]).getPositionAfter();
+		}
+		
+		else {
+			position = new Position();
+		}
+		
+		this._template.set("games." + id + ".board", position.getBoardArray());
+		this._template.set("games." + id + ".last_move", null);
 	}
 	
 	return CurrentGames;
