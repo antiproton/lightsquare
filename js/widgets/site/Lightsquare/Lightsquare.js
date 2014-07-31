25edfe95c12e9285ceb71ae437ec3aef54624f3a
Gus <gus@hogg-blake.co.uk>
2014-07-31 22:20:02 +0100

    refactors Lightsquare init code into helper methods

--- js/widgets/site/Lightsquare/Lightsquare.js
+++ js/widgets/site/Lightsquare/Lightsquare.js
@@ -7,7 +7,7 @@ define(function(require) {
 	var toolsHtml = require("file!./tools.html");
 	var Ractive = require("lib/dom/Ractive");
 	var Router = require("lib/routing/Router");
-	var Path = require("lib/routing/Path");
+	var AddressBarPath = require("lib/routing/AddressBarPath");
 	var TabContainer = require("lib/dom/TabContainer");
 	var Play = require("./_Play/Play");
 	
@@ -16,14 +16,60 @@ define(function(require) {
 	function Lightsquare(user, server, parent) {
 		this._user = user;
 		this._server = server;
-		this._path = new Path();
-		this._router = new Router(this._path);
+		this._router = new Router(new AddressBarPath());
 		
-		var path = this._router.getPath();
+		this._setupTemplate(parent);
+		this._setupRouter();
+		this._setupUser();
 		
-		this._router.PathChanged.addHandler(function(path) {
-			this._template.set("currentPath", path);
+		this._router.execute();
+		
+		new Play(this._user, this._server, this._router.createChild("/play"), this._template.nodes.play);
+	}
+	
+	Lightsquare.prototype._updateUserDetails = function() {
+		this._template.set({
+			username: this._user.getUsername(),
+			userIsLoggedIn: this._user.isLoggedIn()
+		});
+	}
+	
+	Lightsquare.prototype._setupUser = function() {
+		this._server.Connected.addHandler(function() {
+			this._user.getDetails().then((function() {
+				this._updateUserDetails();
+			}).bind(this));
 		}, this);
+		
+		this._user.LoggedIn.addHandler(function() {
+			this._updateUserDetails();
+		}, this);
+		
+		this._user.LoggedOut.addHandler(function() {
+			this._updateUserDetails();
+		}, this);
+	}
+	
+	Lightsquare.prototype._setupRouter = function() {
+		this._router.PathChanged.addHandler(function(path) {
+			this._template.set("currentPath", this._router.getPath());
+		}, this);
+		
+		this._router.addRoute("/", (function() {
+			this._template.set("tab", "/");
+		}).bind(this));
+		
+		this._router.addPartialRoute("/play", (function() {
+			this._template.set("tab", "/play");
+		}).bind(this));
+		
+		this._router.addPartialRoute("/tools", (function() {
+			this._template.set("tab", "/tools");
+		}).bind(this));
+	}
+	
+	Lightsquare.prototype._setupTemplate = function(parent) {
+		var path = this._router.getPath();
 		
 		this._template = new Ractive({
 			el: parent,
@@ -65,43 +111,6 @@ define(function(require) {
 				}
 			}
 		}).bind(this));
-		
-		this._router.addRoute("/", (function() {
-			this._template.set("tab", "/");
-		}).bind(this));
-		
-		this._router.addPartialRoute("/play", (function() {
-			this._template.set("tab", "/play");
-		}).bind(this));
-		
-		this._router.addPartialRoute("/tools", (function() {
-			this._template.set("tab", "/tools");
-		}).bind(this));
-		
-		new Play(this._user, this._server, this._router.createChild("/play"), this._template.nodes.play);
-		
-		this._router.execute();
-		
-		this._server.Connected.addHandler(function() {
-			this._user.getDetails().then((function() {
-				this._updateUserDetails();
-			}).bind(this));
-		}, this);
-		
-		this._user.LoggedIn.addHandler(function() {
-			this._updateUserDetails();
-		}, this);
-		
-		this._user.LoggedOut.addHandler(function() {
-			this._updateUserDetails();
-		}, this);
-	}
-	
-	Lightsquare.prototype._updateUserDetails = function() {
-		this._template.set({
-			username: this._user.getUsername(),
-			userIsLoggedIn: this._user.isLoggedIn()
-		});
 	}
 	
 	return Lightsquare;
