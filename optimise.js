5af269c14042a30af1bf7b909eb9a0731e7306ea
Gus <gus@hogg-blake.co.uk>
2014-08-09 15:48:16 +0100

    adds optimise.js - r.js optimisation config
    run r.js -o optimise.js to create or update lightsquare-optimised
    with optimised javascript

---
+++ optimise.js
@@ -0,0 +1,26 @@
+({
+	appDir: "/home/gus/projects/lightsquare/js",
+	dir: "/home/gus/projects/lightsquare-optimised/js",
+	baseUrl: "./",
+	paths: {
+		"lib": "/home/gus/projects/js",
+		"Array.prototype": "/home/gus/projects/Array.prototype",
+		"tokeniser": "/home/gus/projects/tokeniser",
+		"websocket": "/home/gus/projects/websocket",
+		"dom": "/home/gus/projects/dom",
+		"routing": "/home/gus/projects/routing",
+		"json-local-storage": "/home/gus/projects/json-local-storage",
+		"chess": "/home/gus/projects/chess",
+		"jsonchess": "/home/gus/projects/jsonchess",
+		"require": "/home/gus/projects/lib/require",
+		"ractive": "/home/gus/projects/lib/ractive"
+	},
+	map: {
+		"*": {
+			"css": "require/css",
+			"file": "require/text",
+			"ready": "require/domReady"
+		}
+	},
+	name: "main"
+})
