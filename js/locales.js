26fb20eefe490e0dacd20f113ad2a37ee8817a19
Gus <gus@hogg-blake.co.uk>
2015-01-22 17:07:01 +0000

    adds es locale
    (github - #101)

--- js/locales.js
+++ js/locales.js
@@ -1,7 +1,8 @@
 define(function(require) {
 	var locales = {
 		"de": require("file!./locales/de.json"),
-		"en": require("file!./locales/en.json")
+		"en": require("file!./locales/en.json"),
+		"es": require("file!./locales/es.json")
 	};
 	
 	for(var locale in locales) {
